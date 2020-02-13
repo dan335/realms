76d8b1f43d3ab38d382892e85e005c74635a20f4
Dan <danphi@gmail.com>
2020-02-12 22:18:52 -0800

    active_support

--- commands/about.rb
+++ commands/about.rb
@@ -4,3 +4,6 @@ def command_about(event, mongo)
     str += "https://github.com/dan335/realms"
     event.respond str
 end
+
+
+# https://discordapp.com/oauth2/authorize?client_id=677394191013380116&&scope=bot
