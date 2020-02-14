4c2e77c9bc0b70bbcf35856881b4fefe6d0976e6
Dan <danphi@gmail.com>
2020-02-13 18:43:30 -0800

    about page

--- commands/about.rb
+++ commands/about.rb
@@ -1,9 +1,7 @@
 def command_about(event, mongo)
-    str = "-] **REALMS** [-\n"
+    str = "-] **ABOUT REALMS** [-\n"
     str += "\n"
-    str += "https://github.com/dan335/realms"
+    str += "Add REALMS to your Discord channel with https://discordapp.com/oauth2/authorize?client_id=677394191013380116&&scope=bot\n"
+    str += "Check out the source code at https://github.com/dan335/realms"
     event.respond str
 end
-
-
-# https://discordapp.com/oauth2/authorize?client_id=677394191013380116&&scope=bot
