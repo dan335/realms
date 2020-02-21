6ffa2a9a6a76b8b6013488624bc0302027af627c
github username <github email address>
2020-02-20 22:21:09 -0800

    better deploy script

--- deploy.sh
+++ deploy.sh
@@ -5,3 +5,4 @@ if ! rspec . ; then
 fi
 docker build -t registry.gitlab.com/danphi/realms .
 docker push registry.gitlab.com/danphi/realms
+ssh root@astroe.io 'cd /shipyard/compose; docker-compose pull; docker-compose up -d'
