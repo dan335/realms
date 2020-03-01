6ab34edb8372a8bf4f5f69cf0401268ba94d12a7
Dan <danphi@gmail.com>
2020-03-01 11:12:44 -0800

    formatting

--- commands/winners.rb
+++ commands/winners.rb
@@ -36,7 +36,7 @@ def command_winners(bot, event, mongo)
         str += " "+user[:endedAt].to_formatted_s(:long)+","
 
         hours = ((user[:endedAt] - user[:startedAt]) / 60 / 60).round(1)
-        str += " "+hours.to_s+" hours,"
+        str += "  "+hours.to_s+" hours,"
 
         str += "  "+number_with_commas(user[:networth].round)+" networth,"
 
