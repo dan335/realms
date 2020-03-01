f8d9c0c65401fac59daf735139a7335033590cd0
Dan <danphi@gmail.com>
2020-03-01 10:54:42 -0800

    formatting

--- commands/winners.rb
+++ commands/winners.rb
@@ -33,14 +33,14 @@ def command_winners(bot, event, mongo)
     mongo[:winners].find().sort(:endedAt => -1).skip(page * $settings[:perPage]).limit($settings[:perPage]).each do |user|
         str += counter.to_s+". **"+user[:display_name]+"** -"
 
-        str += user[:endedAt].to_formatted_s(:long)+",  "
+        str += " "+user[:endedAt].to_formatted_s(:long)+","
 
         hours = ((user[:endedAt] - user[:startedAt]) / 60 / 60).round(1)
         str += " "+hours.to_s+" hours,"
 
-        str += " "+number_with_commas(user[:networth].round)+" networth,"
+        str += " "+number_with_commas(user[:networth].round)+"  networth,"
 
-        str += " "+number_with_commas(user[:population])+" population"
+        str += " "+number_with_commas(user[:population])+"  population"
 
         str += "\n"
     end
