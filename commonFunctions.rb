07ead89d838989b3e0955905f469f7769f2296e4
Dan <danphi@gmail.com>
2020-03-04 19:10:41 -0800

    fix feedArmies

--- commonFunctions.rb
+++ commonFunctions.rb
@@ -287,7 +287,7 @@ def feedArmies(bot, mongo)
             end
 
             # clamp, 0.95 - 1.0, max 5% of soldiers
-            killPercentage = 1.0 - [[percentage, 0.95].min, 1.0].max
+            killPercentage = 1.0 - [[percentage, 0.95].max, 1.0].min
 
             # get total number of soldiers
             numSoldiers = 0
