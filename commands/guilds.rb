2a0598f5d2d78a8e4715e8f3e31ab8d88e0b227c
github username <github email address>
2020-06-19 19:08:55 -0700

    paginate guilds command

--- commands/guilds.rb
+++ commands/guilds.rb
@@ -1,9 +1,35 @@
 def command_guilds(bot, event, mongo)
-    str = "-] **REALMS GUILDS** [-\n\n"
-    
-    count = 1
+    str = "-] **REALMS GUILDS** [-\n"
+
+    # find page number
+    page = 0
+
+    msg = event.message.content
+    msg.slice!(0)
+    arr = msg.split
+
+    if arr.length > 1
+        t = arr[1].to_i
+        if t == 0
+            event.respond "Page "+arr[1]+" is not a valid page number "+event.message.author.mention+"."
+            return
+        end
+
+        page = t - 1
+    end
+
+    # find number of pages
+    str += "page "+(page+1).to_s+" of "+(bot.servers.length().to_f / $settings[:perPage].to_f).ceil.to_s+"\n"
+    str += "\n"
+
+    # print guilds
+    count = 0
     bot.servers.each do |num, server|
-        str += count.to_s+". "+server.name+" - "+server.member_count.to_s+" members\n"
+        if count >= page * $settings[:perPage]
+            if count < page * $settings[:perPage] + $settings[:perPage]
+                str += (count+1).to_s+". "+server.name+" - "+server.member_count.to_s+" members\n"
+            end
+        end
         count += 1
     end
 
